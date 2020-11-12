a1731a3f549235b7c2c33f5665d934c0a8c53d81
Pedro Paulo <52174779+PedroPaulo-Faria@users.noreply.github.com>
2020-11-12 17:17:16 -0300

    Create Retangulo2.java

---
+++ programas java/Retangulo2.java
@@ -0,0 +1,8 @@
+public class Retangulo2 extends Quadrilatero
+{
+	public Retangulo2 (double lado1,double lado2)
+	{
+		super(lado1,lado2); // Repassando argumentos para a superclasse
+		super.ID = "Retângulo";
+	}
+}
