470d904f51c3e260f2ddaee29aaaf961d0c36f45
Pedro Paulo <52174779+PedroPaulo-Faria@users.noreply.github.com>
2020-11-12 17:22:54 -0300

    Delete Retangulo2.java

--- programas java/Retangulo2.java
+++
@@ -1,8 +0,0 @@
-public class Retangulo2 extends Quadrilatero
-{
-	public Retangulo2 (double lado1,double lado2)
-	{
-		super(lado1,lado2); // Repassando argumentos para a superclasse
-		super.ID = "Retângulo";
-	}
-}
